51589b1b893524d92be14cb5cc9baf9f90f1518d
Taketomo ohishi <takeweb@mac.com>
2013-05-16 00:20:59 +0900

    add fizzbuzz

---
+++ 99.turtle/square.py
@@ -0,0 +1,13 @@
+import 
+
+year = 2013
+base = "2013年度"
+for cnt in range(4, 16):
+    if cnt > 12:
+        month = cnt - 12
+        if cnt == 13:
+            year += 1
+    else:
+        month = cnt
+    month = str(month).zfill(2)
+    os.makedirs( base + "/" + str(year) + "年" + str(month) + "月" )
