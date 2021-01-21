288374cb4b941c9a1851f7eb3eebecd61474ecaa
Taketomo Oishi <oishi@multisoup.co.jp>
2021-01-21 17:11:28 +0900

    createDDL2.pyの内容を取り込み

--- 07_Excel操作/01_DDL生成/createDDL.py
+++ 07_Excel操作/01_DDL生成/createDDL.py
@@ -5,28 +5,39 @@ import sys, argparse
 from openpyxl import load_workbook
 
 def createDDL(filename):
+    # DDL生成
+    # A5:SQL MkⅡが出力する形式のテーブル定義書対応
+    ROW_HEADER_MIN = 4
+    ROW_DETAIL_MIN = 13
     wb = load_workbook(filename, read_only=True, data_only=True)
     sheet_list = wb.sheetnames
     for sheet_name in sheet_list:
         col_info_list = []
+        if sheet_name == "テーブル一覧":
+            continue
         ws = wb[sheet_name]
+        end_flg = False
         for i, row in enumerate(ws):
             col_info = {}
             name = ""
-            for j, col in enumerate(row):
-                if j == 3:
-                    if i == 1:
-                        table_name = str(col.value).rstrip()
-                    elif i == 2:
-                        table_remarks = str(col.value).rstrip() 
- 
+            if i == ROW_HEADER_MIN:
+                table_remarks = str(row[2].value).rstrip()
+            elif i == ROW_HEADER_MIN + 1:
+                table_name = str(row[2].value).rstrip()
+            
+            for j, col in enumerate(row): 
+
                 if col.value is not None:
                     if col.data_type == "d":
                         value = col.value
                     else:
                         value = str(col.value).rstrip()
 
-                    if i >= 6:
+                    if i >= ROW_DETAIL_MIN and end_flg == False:
+                        if value == "インデックス名":
+                            end_flg = True
+                            break
+
                         if j == 1:
                             col_info["remarks"] = value
                         elif j == 2:
@@ -35,15 +46,17 @@ def createDDL(filename):
                         elif j == 3:
                             col_info["data_type"] = value
                         elif j == 4:
-                            col_info["pk"] = "  , PRIMARY KEY (" + name + ")"
-                        elif j == 5:
                             col_info["not_null"] = " NOT NULL"
-                        elif j == 6:
+                            if value.find("PK") > -1:
+                                col_info["pk"] = "  , PRIMARY KEY (" + name + ")"
+                        elif j == 5 and value != "":
                             col_info["default"] = " DEFAULT " + value
                         #print(f'data:{value}')
             if len(col_info) > 0:
                 #print(col_info)
                 col_info_list.append(col_info)
+            if end_flg:
+                break
         #print(table_name)
         #print(table_remarks)
         #print(col_info_list)
